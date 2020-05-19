7281cd655f934c07eb3e0e2feb04834da199f061
humphreymabeya <humphreymabeya@gmail.com>
2020-05-19 16:50:33 +0300

    stripe checkout

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
+const stripe = require('stripe')('sk_test_H4Psa65bkCc9n3Tkj9QoWZwl00lpf7jcRJ');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.CheckoutSession = functions.https.onRequest((request, response) => {
+    cors(req, res, () => {
+        stripe.checkout.sessions.create(
+                {
+                success_url: 'https://example.com/success',
+                cancel_url: 'https://example.com/cancel',
+                payment_method_types: ['card'],
+                line_items: [
+                    {
+                    price: 'price_H5ggYwtDq4fbrJ',
+                    quantity: 2,
+                    },
+                ],
+                },
+                function(err, session) {
+                // asynchronously called
+                response.send(session);
+                }
+            );
+    });
+    
+});
+   
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -6,33 +6,76 @@
 				<div class="col-md-8">
 					<h4 class="py-4">Checkout Page</h4>
 					<ul>
-							<li v-for="item in this.$store.state.cart" class="media">
-								<img :src="item.productImage" width="80px" class="align-self-center mr-3" alt="">
-								<div class="media-body">
-									<h5 class="mt-0">{{item.productName}}
-										<span class='float-right btn' @click="$store.commit('removeFromCart',item)"><i class="fa fa-times-circle"></i></span>
-									</h5>
-									<p class="mt-0">{{item.productPrice | currency('Kshs. ')}}</p>
-									<p class="mt-0">Quantity : {{item.productQuantity }}</p>
-								</div>
-							</li>
-				  	</ul>
+						<li v-for="item in this.$store.state.cart" class="media">
+							<img :src="item.productImage" width="80px" class="align-self-center mr-3" alt="">
+							<div class="media-body">
+								<h5 class="mt-0">{{item.productName}}
+									<span class='float-right btn' @click="$store.commit('removeFromCart',item)"><i class="fa fa-times-circle"></i></span>
+								</h5>
+								<p class="mt-0">{{item.productPrice | currency('Kshs. ')}}</p>
+								<p class="mt-0">Quantity : {{item.productQuantity }}</p>
+							</div>
+						</li>
+				  </ul>
 				</div>
 				<div class="col-md-4">
 					<h5>Total Price : {{ this.$store.getters.totalPrice | currency('Kshs. ') }}</h5>
-					<card class='stripe-card'
-            :class='{ complete }'
-            stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
-            :options='stripeOptions'
-            @change='complete = $event.complete'
-          />
-          	<button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>                
-          </form>
+					<!-- <card class='stripe-card'
+						:class='{ complete }'
+						stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
+						:options='stripeOptions'
+						@change='complete = $event.complete'
+					/>
+          			<button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>                
+          			</form> -->
+					<button class="btn btn-primary" @click="pay">Checkout</button>
 				</div>
 			</div>
 		</div>
   </div>
 </template>
+<script>
+import axios from 'axios';
+
+var stripe = Stripe('pk_test_xRwVki7TILOExRzi2IKTnNn000vwTRK9pY');
+
+export default {
+	data(){
+		return{
+			sessionId: ''
+		}
+	},
+	methods: {
+		pay(){
+			// data = {id:10,id:10}
+			let data = this.$store.state.cart.map(item => ({ [item.productId] : item.productQuantity}));
+			data = Object.assign({}, ...data);
+			
+			axios.get('https://us-central1-vue-shop-a745d.cloudfunctions.net/CheckoutSession', {
+              	params: {
+                  	products : data
+              	}
+          	})
+            .then(response => {
+                this.sessionId = response.data
+                console.log(response.data);
+                stripe.redirectToCheckout({
+                    sessionId: this.sessionId.id
+                }).then(function (result) {
+                    
+                    });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+		}
+	},
+	created(){
+		
+	}
+}
+</script>
 <style scoped>
 	.StripeElement {
 		box-sizing: border-box;
